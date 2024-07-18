feb5002cacd300475c44aba64170671638c7d45d
nathankr <natankrasney@gmail.com>
2024-07-18 06:55:07 +0300

    remove gitignore on speech output

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,11 @@ const speechFile = path.resolve('speech',"./speech.mp3");
 async function main() {
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
-    voice: "alloy",
-    input: "Today is a wonderful day to build something people love!",
+    voice: "alloy", // -- there are 6 possible voices
+    input: "Nathan Krasney said : Today is a wonderful day to build something people love!",
   });
   const buffer = Buffer.from(await mp3.arrayBuffer());
   await fs.promises.writeFile(speechFile, buffer);
-  console.log(`file write completed : ${speechFile}`);
+  console.log(`audio file write completed : ${speechFile}`);
 }
 main();
